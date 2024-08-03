70996363c252c000f18e76f21eea0ac895fdfa2d
Pdubey1994 <committeeurban@gmail.com>
2024-08-03 20:26:49 +0530

    initial commit

---
+++ src/main/java/com/urban/committee/UcUserOnboardingAppApplication.java
@@ -0,0 +1,13 @@
+package com.urban.committee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcUserOnboardingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UcUserOnboardingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/urban/committee/UcUserOnboardingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urban.committee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcUserOnboardingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
